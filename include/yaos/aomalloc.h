a9d43cb6d2c29e626eccbc145aa08ca97752afe8
saneee <399030@qq.com>
2015-11-08 17:39:01 +0800

    include file

---
+++ include/yaos/aomalloc.h
@@ -0,0 +1,10 @@
+#ifndef __YAOS_ALLOCONLY_H
+#define __YAOS_ALLOCONLY_H
+struct ao_malloc{
+    void * phead;
+    
+};
+void aomem_init(struct ao_malloc *p);
+void aomem_free(struct ao_malloc *pobj,void *p,ulong size);
+void * aomem_alloc(struct ao_malloc *pobj,ulong size);
+#endif
---
+++ include/yaos/printk.h
@@ -0,0 +1,8 @@
+#ifndef __YAOS_PRINTK__H
+#define __YAOS_PRINTK__H 1
+int sprintf(char *buf, const char *fmt, ...);
+int printf(const char *fmt, ...);
+int printk(const char *fmt, ...);
+void panic(const char *fmt, ...);
+static inline void inline_printk(const char *fmt,...){};
+#endif
