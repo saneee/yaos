a6131093c958f1d9d8e63f276cd04c96aa0cd9c1
saneee <399030@qq.com>
2015-11-27 22:09:03 +0800

    string

---
+++ libs/string/strchrnul.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#ifndef UCHAR_MAX
+#define UCHAR_MAX ((unsigned char )~0U)
+#endif
+
+#define ALIGN (sizeof(size_t))
+#define ONES ((size_t)-1/UCHAR_MAX)
+#define HIGHS (ONES * (UCHAR_MAX/2+1))
+#define HASZERO(x) (((x)-ONES) & ~(x) & HIGHS)
+
+char *__strchrnul(const char *s, int c)
+{
+	size_t *w, k;
+
+	c = (unsigned char)c;
+	if (!c) return (char *)s + strlen(s);
+
+	for (; (uintptr_t)s % ALIGN; s++)
+		if (!*s || *(unsigned char *)s == c) return (char *)s;
+	k = ONES * c;
+	for (w = (void *)s; !HASZERO(*w) && !HASZERO(*w^k); w++);
+	for (s = (void *)w; *s && *(unsigned char *)s != c; s++);
+	return (char *)s;
+}
+#undef weak_alias
+#define weak_alias(old, new) \
+        extern __typeof(old) new __attribute__((weak, alias(#old)))
+
+
+weak_alias(__strchrnul, strchrnul);
